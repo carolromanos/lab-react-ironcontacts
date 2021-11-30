96fa172f9d7d54bcd40e8982151f7a937b7aa098
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-11-30 21:29:59 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsJSON from "./contacts.json"
+import React, { useState } from "react";
+
+const remainingContacts = [...contactsJSON]
+const initAgenda = remainingContacts.splice(0, 5)
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [agenda, setAgenda] = useState(initAgenda);
 
+  const addContact = ()=>{
+    const randomIndex = Math.floor(Math.random() * remainingContacts.length)
+    const randomContact = remainingContacts.splice(randomIndex, 1)
+  
+    setAgenda(agenda.concat(randomContact))
+   //setAgenda([ ...agenda, randomContact ])
+  }
+
+  const sortByName = ()=>{
+    let sortContacts = agenda.sort((a, b) => a.name.localeCompare(b.name));
+    setAgenda([...sortContacts]);
+  }
+
+  const sortByPopularity = ()=>{
+    let sortContacts = agenda.sort((a, b)=> b.popularity - a.popularity)
+    setAgenda([...sortContacts])
+  }
+  const removeContact =(index)=>{
+    agenda.splice(index, 1)
+    setAgenda([...agenda])
+  }
+
+ return <div className="App">
+ <h1>IRONCONTACTS</h1>
+ <button onClick={addContact}>Add random contact</button>
+ <button onClick={sortByName}>Sort by name</button>
+ <button onClick={sortByPopularity}>Sort by popularity</button>
+
+
+    <table >
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+        <th>Oscar Winner?</th>
+        <th>Oscar Emmy?</th>
+
+      </tr>
+    </thead>
+
+      <tbody>
+        {agenda.map(contact =>{
+          return(
+            <tr key={contact.id}>
+              <td>
+                <img src={contact.pictureUrl} alt={contact.name} width="100px"/>
+              </td>
+              <td>
+                <p>{contact.name}</p>
+              </td>
+              <td>
+                <p>{contact.popularity.toFixed(2)}</p>
+              </td>
+              <td>
+                <p>{contact.wonOscar ? "🏆" : "X"}</p>
+              </td>
+              <td>
+                <p>{contact.wonEmmy ? "🏆" : "X"}</p>
+              </td>
+              <td>
+              <button onClick={(i) => removeContact(i)}>Remove contact</button>              </td>
+            </tr>
+
+          )
+        })}
+      </tbody>
+  </table>
+</div>;
+}
 export default App;
