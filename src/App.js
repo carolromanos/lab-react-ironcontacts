c3d0bf26cdcd670596f417101e828d1b82c2dbfc
Carolina Romanos <carolina.romanos.roca@gmail.com>
2021-12-01 09:28:28 +0100

    format

--- src/App.js
+++ src/App.js
@@ -9,53 +9,52 @@ const initAgenda = remainingContacts.splice(0, 5)
 function App() {
   const [agenda, setAgenda] = useState(initAgenda);
 
-  const addContact = ()=>{
+  const addContact = () => {
     const randomIndex = Math.floor(Math.random() * remainingContacts.length)
     const randomContact = remainingContacts.splice(randomIndex, 1)
-  
+
     setAgenda(agenda.concat(randomContact))
-   //setAgenda([ ...agenda, randomContact ])
+    //setAgenda([ ...agenda, randomContact ])
   }
 
-  const sortByName = ()=>{
+  const sortByName = () => {
     let sortContacts = agenda.sort((a, b) => a.name.localeCompare(b.name));
     setAgenda([...sortContacts]);
   }
 
-  const sortByPopularity = ()=>{
-    let sortContacts = agenda.sort((a, b)=> b.popularity - a.popularity)
+  const sortByPopularity = () => {
+    let sortContacts = agenda.sort((a, b) => b.popularity - a.popularity)
     setAgenda([...sortContacts])
   }
-  const removeContact =(index)=>{
+  const removeContact = (index) => {
     agenda.splice(index, 1)
     setAgenda([...agenda])
   }
 
- return <div className="App">
- <h1>IRONCONTACTS</h1>
- <button onClick={addContact}>Add random contact</button>
- <button onClick={sortByName}>Sort by name</button>
- <button onClick={sortByPopularity}>Sort by popularity</button>
-
+  return <div className="App">
+    <h1>IRONCONTACTS</h1>
+    <button onClick={addContact}>Add random contact</button>
+    <button onClick={sortByName}>Sort by name</button>
+    <button onClick={sortByPopularity}>Sort by popularity</button>
 
     <table >
-    <thead>
-      <tr>
-        <th>Picture</th>
-        <th>Name</th>
-        <th>Popularity</th>
-        <th>Oscar Winner?</th>
-        <th>Oscar Emmy?</th>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Oscar Winner?</th>
+          <th>Oscar Emmy?</th>
 
-      </tr>
-    </thead>
+        </tr>
+      </thead>
 
       <tbody>
-        {agenda.map(contact =>{
-          return(
+        {agenda.map(contact => {
+          return (
             <tr key={contact.id}>
               <td>
-                <img src={contact.pictureUrl} alt={contact.name} width="100px"/>
+                <img src={contact.pictureUrl} alt={contact.name} width="100px" />
               </td>
               <td>
                 <p>{contact.name}</p>
@@ -70,13 +69,13 @@ function App() {
                 <p>{contact.wonEmmy ? "🏆" : "X"}</p>
               </td>
               <td>
-              <button onClick={(i) => removeContact(i)}>Remove contact</button>              </td>
+                <button onClick={(i) => removeContact(i)}>Remove contact</button>              </td>
             </tr>
 
           )
         })}
       </tbody>
-  </table>
-</div>;
+    </table>
+  </div>;
 }
 export default App;
